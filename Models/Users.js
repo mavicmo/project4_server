0add339bd41a94a5d61f8b829bfbb14406337e66
mtaraq5@gmail.com <mtaraq@Mos-MacBook-Pro.fios-router.home>
2022-07-12 23:53:19 -0400

    made changes and removed changes for the backend

--- Models/Users.js
+++ Models/Users.js
@@ -5,20 +5,17 @@ const Users = new Schema(
   {
     firstName: {
       type: String,
-      required: [true, "First Name is required"],
     },
     lastName: {
       type: String,
-      required: [true, "Last Name is required"],
     },
     email: {
       type: String,
-      required: [true, "Email is required"],
+
       unique: true,
     },
     password: {
       type: String,
-      required: [true, "Password is required"],
     },
     Months: {
       type: [String],
